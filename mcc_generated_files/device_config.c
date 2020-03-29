f54cc0e6c820cb01b90809af539747466af621d5
Aleix Sanchis <aleixsanchis@hotmail.com>
2020-03-29 13:43:58 +0200

    disabled Brown-Out Reset

--- mcc_generated_files/device_config.c
+++ mcc_generated_files/device_config.c
@@ -18,12 +18,10 @@
 
 // CONFIG
 #pragma config FOSC = INTOSC    // Oscillator Selection bits->INTOSC oscillator: CLKIN function disabled
-#pragma config BOREN = ON    // Brown-out Reset Enable->Brown-out Reset enabled
+#pragma config BOREN = OFF    // Brown-out Reset Enable->Brown-out Reset enabled
 #pragma config WDTE = OFF    // Watchdog Timer Enable->WDT disabled
 #pragma config PWRTE = OFF    // Power-up Timer Enable bit->PWRT disabled
 #pragma config MCLRE = OFF    // MCLR Pin Function Select bit->MCLR pin function is digital input, MCLR internally tied to VDD
 #pragma config CP = OFF    // Code Protection bit->Program memory code protection is disabled
 #pragma config LVP = OFF    // Low-Voltage Programming Enable->High-voltage on MCLR/VPP must be used for programming
-#pragma config LPBOR = ON    // Brown-out Reset Selection bits->BOR enabled
-#pragma config BORV = LO    // Brown-out Reset Voltage Selection->Brown-out Reset Voltage (Vbor), low trip point selected.
 #pragma config WRT = OFF    // Flash Memory Self-Write Protection->Write protection off
